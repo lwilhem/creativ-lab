5cf11e4e522638e10f9f36e4e5886bd065770e07
Rayan - Laptop <rayan.idri@edu.devinci.fr>
2022-04-22 16:48:53 +0200

    Enfin fini git add .!

--- src/Controller/ImpressionController.php
+++ src/Controller/ImpressionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\PostsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,14 @@ use Symfony\Component\Routing\Annotation\Route;
 class ImpressionController extends AbstractController
 {
     #[Route('/impression', name: 'impression')]
-    public function index(): Response
+    public function index(PostsRepository $postsRepository): Response
     {
+
+        $post = $postsRepository->findBy([],['createdAt'=>'ASC']);
+
         return $this->render('impression/impression.html.twig', [
             'controller_name' => 'ImpressionController',
+            'posts' => $post,
         ]);
     }
 }
