27c9264da615486e7b61ccfdde761e73cb57519a
lwilhem <59793152+lwilhem@users.noreply.github.com>
2022-04-22 16:05:08 +0200

    :rocket: Single Project Page

--- src/Controller/DetailedPageController.php
+++ src/Controller/DetailedPageController.php
@@ -2,17 +2,21 @@
 
 namespace App\Controller;
 
+use App\Repository\ProjectsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DetailedPageController extends AbstractController
 {
-    #[Route('/detailed/page', name: 'detailed_page')]
-    public function index(): Response
+    #[Route('/detailed/{id}', name: 'detailed_page')]
+    public function index(ProjectsRepository $projectsRepository, $id): Response
     {
+        $data = $projectsRepository->findOneBy(['id' => $id]);
+
         return $this->render('detailed_page/detailed_page.html.twig', [
             'controller_name' => 'DetailedPageController',
+            'data' => $data
         ]);
     }
 }
